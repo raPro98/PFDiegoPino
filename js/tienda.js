72defa0bb65d190e874b88c2a3a06c90547989bd
DiegoPM <diego.pino.miranda@gmail.com>
2024-05-02 18:11:22 -0400

    se agrega fetch

--- js/tienda.js
+++ js/tienda.js
@@ -1,5 +1,21 @@
 // PRODUCTOS
-let productosTienda = JSON.parse(localStorage.getItem('productos Tienda'));
+let productosWaterflame = [];
+
+fetch("../js/productos.json")
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Hubo un error al cargar los datos.');
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        productosWaterflame = data;
+        cargaProductos(productosWaterflame);
+    }).catch(error =>{
+        console.error('Error al cargar los productos:', error.message)
+    });
+
 
 // VARIABLES GLOBALES
 const contenedorProductos = document.querySelector('.contenedorProductos');
@@ -36,7 +52,7 @@ cargaProductos =(tipoProductos)=>{
         btnAgregar.forEach(boton => {
             boton.addEventListener('click', (e)=>{
                 let idBtn = e.currentTarget.id;
-                let productoAgregado = productosTienda.find(producto => producto.id === idBtn);
+                let productoAgregado = productosWaterflame.find(producto => producto.id === idBtn);
 
                     // AUMENTAR LA CANTIDAD DEL PRODUCTO SI ES QUE YA EXISTE EN EL CARRITO
                 if(productosCarrito.some(productoExistente => productoExistente.id === idBtn)){
@@ -88,100 +104,10 @@ btnCategoria.forEach(btn =>{
         let idProductos;
 
         if(e.currentTarget.id != 'todo'){
-            idProductos = productosTienda.filter(producto => producto.categoria.id === e.currentTarget.id);
+            idProductos = productosWaterflame.filter(producto => producto.categoria.id === e.currentTarget.id);
             cargaProductos(idProductos)
         } else{
-            cargaProductos(productosTienda)
+            cargaProductos(productosWaterflame)
         }
     })
 });
-
-cargaProductos(productosTienda);
-
-
-
-
-
-
-
-// const buscarCancionForm = document.getElementById('buscarCancion-form');
-// const buscarCancion = document.getElementById('buscarCancion');
-// const topPropio = document.getElementById('topPropio');
-// const contenedorForm = document.querySelector('.contenedor-form');
-
-// agregarCancion = (e) => {
-//     let canciones = readText('canciones.json');
-//     let interpreteCanciones = JSON.parse(canciones)[0].canciones;
-
-//     if (buscarCancion.value.trim()) {
-//         let cancionesFiltradas = interpreteCanciones.filter(cancion => {
-//             return cancion.nombre.toLowerCase().includes(buscarCancion.value.toLowerCase());
-//         });
-
-//         if (cancionesFiltradas.length > 0) {
-//             let contenedor = document.createElement('div');
-//             cancionesFiltradas.forEach(cancion => {
-
-//                 let contenedor = document.createElement('div');
-//                 contenedor.innerHTML = `
-//                     <h3 class="tituloCanciones">${cancion.nombre}.</h3>
-//                     <a href="${cancion.enlaces.spotify}" class="p-0 m-0" target="_blank">
-//                         <img src="${cancion.imagen}" alt="Imagen de la cancion ${cancion.nombre}">
-//                     </a>
-//                     <p class="fw-medium m-0 mt-2">- Álbum: ${cancion.album}.</p>
-//                     <p class="escuchalo">Escuchalo en:</p>
-//                     <ul class="list-unstyled d-flex justify-content-md-between justify-content-around flex-wrap row-gap-2 px-0">
-//                         <li>
-//                             <a href="${cancion.enlaces.youtube}" class="btn btn-primary px-0 btnMusica btnCanciones btnYoutube" target="_blank">
-//                                 <i class="bi bi-youtube"></i>
-//                                 YouTube
-//                             </a>
-//                         </li>
-//                         <li>
-//                             <a href="${cancion.enlaces.spotify}" class="btn btn-primary float-md-none float-end px-0 btnMusica btnCanciones btnSpotify" target="_blank">
-//                                 <i class="bi bi-spotify"></i>
-//                                 Spotify
-//                             </a>
-//                         </li>
-//                         <li>
-//                             <a href="${cancion.enlaces.newgrounds}" class="btn btn-primary btnMusica btnCanciones btnNewgrounds" target="_blank">
-//                                 <img src="../img/logonewgrounds.png" class="logoNewgrounds" alt="Logo Newgrounds">
-//                                 NG
-//                             </a>
-//                         </li>
-//                     </ul>
-//                 `;
-//                 contenedor.appendChild(cancionHTML);
-//                 contenedorForm.style.display = 'none';
-//             });
-//             topPropio.appendChild(contenedor);
-            
-//         } else {
-//             alert('No se encontraron canciones que coincidan con la búsqueda.');
-//         }
-//     };
-
-//     e.preventDefault();
-//     buscarCancionForm.reset();
-// };
-// //         topPropio.appendChild(contenedor);
-// //         contenedorForm.style.display = 'none';
-// //     };
-
-// //     e.preventDefault();
-// //     buscarCancionForm.reset();
-
-// // };
-
-// buscarCancionForm.addEventListener('submit', agregarCancion);
-
-// function readText(rutaLocal) {
-//     var texto = null;
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.open("GET", rutaLocal, false);
-//     xmlhttp.send();
-//     if (xmlhttp.status == 200) {
-//         texto = xmlhttp.responseText;
-//     }
-//     return texto;
-// }
